98a7d0924535e4977534d0b3c99c825a571608ea
PlaySomeJazz <cokepizzachocolate@gmail.com>
2023-11-09 11:22:35 +0200

    fixed colors

--- config.h
+++ config.h
@@ -113,7 +113,7 @@ float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#000000", /* hard contrast: #1d2021 / soft contrast: #32302f */
 	"#cc241d",
 	"#98971a",
 	"#d79921",
@@ -121,7 +121,7 @@ static const char *colorname[] = {
 	"#b16286",
 	"#689d6a",
 	"#a89984",
-	"#928374",
+	"#282828",
 	"#fb4934",
 	"#b8bb26",
 	"#fabd2f",
